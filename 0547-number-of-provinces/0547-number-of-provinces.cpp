c37790951b4bf24367a2aafca80167ff93cb49e0
madhesh281207 <madhesh281207@gmail.com>
2026-07-28 11:32:00 +0530

    Time: 0 ms (100%), Space: 19.5 MB (60.99%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    vector<int>parent;
+    vector<int>rank;
+public:
+    int findParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+        return parent[node]=findParent(parent[node]);
+    }
+    void joinParent(int u,int v){
+        int pu=findParent(u);
+        int pv=findParent(v);
+        if(pu==pv) return;
+        if(rank[pu]<rank[pv]){
+            parent[pu]=pv;
+        }else if(rank[pu]>rank[pv]){
+            parent[pv]=pu;
+        }else{
+            parent[pv]=pu;
+            pu++;
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int N=isConnected.size();
+        parent.resize(N+1);
+        rank.resize(N+1,0);
+        for(int i=0;i<=N;i++){
+            parent[i]=i;
+        }
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                if(isConnected[i][j]==1){
+                    if(i<j){
+                        joinParent(i,j);
+                    }
+                }
+            }
+        }
+        unordered_set<int>st;
+        for(int i=0 ; i<N ; i++){
+            st.insert(findParent(i));
+        }
+        return st.size();
+
+    }
+};
